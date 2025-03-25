6f28ed9ac25355b16349c93b2db4850c72746448
JasperHv <jbvanheusden@gmail.com>
2025-03-25 22:26:04 +0100

    Implement Iterator and Comparable design patterns for card(deck)

--- src/main/java/nl/vu/cs/softwaredesign/data/model/Card.java
+++ src/main/java/nl/vu/cs/softwaredesign/data/model/Card.java
@@ -1,8 +1,9 @@
 package nl.vu.cs.softwaredesign.data.model;
 
 import java.util.List;
+import java.util.Objects;
 
-public class Card {
+public class Card implements Comparable<Card> {
     private int id;
     private String title;
     private String scenario;
@@ -33,4 +34,27 @@ public class Card {
     public Card decrementFrequency() {
         return new Card(this.id, this.title, this.scenario, this.pillar, this.type, this.influence, this.frequency - 1);
     }
+
+    @Override
+    public int compareTo(Card other) {
+        return Integer.compare(other.frequency, this.frequency);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Card other = (Card) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
--- src/main/java/nl/vu/cs/softwaredesign/data/model/CardDeck.java
+++ src/main/java/nl/vu/cs/softwaredesign/data/model/CardDeck.java
@@ -2,21 +2,25 @@ package nl.vu.cs.softwaredesign.data.model;
 
 import java.util.*;
 
-public class CardDeck {
+public class CardDeck implements Iterable<Card> {
     private List<Card> cards;
 
+    /**
+     * Constructor to initialize the deck with a list of cards.
+     * The deck is sorted and shuffled upon initialization.
+     * @param initialCards The initial list of cards to populate the deck.
+     */
     public CardDeck(List<Card> initialCards) {
         this.cards = new ArrayList<>(initialCards);
         sortAndShuffleDeck();
     }
-
 
     /**
      * Sorts the deck by card frequency in descending order, groups cards by frequency,
      * shuffles within each group, and then rebuilds the deck.
      */
     public void sortAndShuffleDeck() {
-        cards.sort((c1, c2) -> Integer.compare(c2.getFrequency(), c1.getFrequency()));
+        cards.sort(Card::compareTo);
 
         Map<Integer, List<Card>> frequencyGroups = new HashMap<>();
         for (Card card : cards) {
@@ -28,7 +32,7 @@ public class CardDeck {
                 .sorted(Comparator.reverseOrder())
                 .forEach(freq -> {
                     List<Card> group = frequencyGroups.get(freq);
-                    Collections.shuffle(group);
+                    Collections.shuffle(group); // Shuffle within each group
                     newDeck.addAll(group);
                 });
 
@@ -43,6 +47,7 @@ public class CardDeck {
         if (isEmpty()) {
             return null;
         }
+
         Card selectedCard = null;
 
         while (!cards.isEmpty()) {
@@ -65,7 +70,40 @@ public class CardDeck {
         return null;
     }
 
+    /**
+     * Checks if the deck is empty.
+     * @return true if the deck is empty, otherwise false.
+     */
     public boolean isEmpty() {
         return cards.isEmpty();
     }
+
+    /**
+     * Provides an iterator to allow iteration over the deck.
+     * @return an Iterator for the deck of cards.
+     */
+    @Override
+    public Iterator<Card> iterator() {
+        return new Iterator<Card>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < cards.size();
+            }
+
+            @Override
+            public Card next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return cards.get(index++);
+            }
+        };
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
 }
+
