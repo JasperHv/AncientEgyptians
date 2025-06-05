aaf356fff37f7cce0cfbbe629c87232e4589faae
Jasper van Heusden Laptop <jbvanheusden@gmail.com>
2025-06-05 12:47:16 +0200

    Create GameStateEntry and logger, based on CommandLogEntry and logger

---
+++ src/main/java/nl/vu/cs/ancientegyptiansgame/logging/GameStateEntry.java
@@ -0,0 +1,35 @@
+package nl.vu.cs.ancientegyptiansgame.logging;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+public class GameStateEntry {
+    @JsonProperty("pillarValues")
+    private final Map<String, Integer> pillars;
+
+    @JsonProperty("year")
+    private final int year;
+
+    @JsonProperty("score")
+    private final int score;
+    @JsonProperty("timestamp")
+    private final long timestamp;
+
+    protected GameStateEntry(Map<String, Integer> pillars, int year, int score, long timestamp) {
+        this.pillars = pillars;
+        this.year = year;
+        this.score = score;
+        this.timestamp = timestamp;
+    }
+
+    public Map<String, Integer> getPillars() { return pillars; }
+
+    public int getYear() { return year; }
+
+    public int getScore() { return score; }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
---
+++ src/main/java/nl/vu/cs/ancientegyptiansgame/logging/GameStateLogger.java
@@ -0,0 +1,37 @@
+package nl.vu.cs.ancientegyptiansgame.logging;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GameStateLogger {
+    private static final String LOG_FILE = "src/main/resources/configuration/gamestate.json";
+    private static final List<GameStateEntry> logEntries = new ArrayList<>();
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE);
+    private static final Logger logger = Logger.getLogger(GameStateLogger.class.getName());
+
+    private GameStateLogger() {}
+
+    public static void logCommand(GameStateEntry entry) {
+        logEntries.add(entry);
+        saveLogToFile();
+    }
+
+    private static void saveLogToFile() {
+        try {
+            mapper.writeValue(new File(LOG_FILE), logEntries);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error saving gamestate log to file", e);
+        }
+    }
+}
+
