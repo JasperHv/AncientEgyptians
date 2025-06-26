20ea0d91ae3c449d93013fca2277ccd96b0bf642
Jasper van Heusden Laptop <jbvanheusden@gmail.com>
2025-06-26 11:50:32 +0200

    test: implement endings parsing tests

--- src/test/java/config/ConfigurationLoaderUnitTests.java
+++ src/test/java/config/ConfigurationLoaderUnitTests.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import ancientegyptiansgame.data.model.Mode;
+import ancientegyptiansgame.data.model.Ending;
 
 class ConfigurationLoaderUnitTests {
 
@@ -73,5 +74,36 @@ class ConfigurationLoaderUnitTests {
                 mapper.getTypeFactory().constructCollectionType(List.class, String.class)
         );
         assertTrue(monarchs.isEmpty());
+    }
+
+
+    @Test
+    void parseGoldenAgeEnding_ValidJson_ReturnsCorrectEnding() throws Exception {
+        String json = "{ \"goldenAgeEnding\": { " +
+                "\"description\": \"Glorious Pharaoh, your rule is unmatched! Through wisdom, balance, and strong leadership, you have ushered in a Golden Age for Egypt. The people prosper, the temples shine, and your name will be spoken with reverence for centuries to come. Your dynasty stands eternal, a beacon of prosperity and order.\"," +
+                "\"image\": \"endings/golden-age-ending.png\" } }";
+        ObjectMapper mapper = new ObjectMapper();
+
+        Ending goldenAgeEnding = mapper.convertValue(
+                mapper.readTree(json).get("goldenAgeEnding"),
+                Ending.class
+        );
+        assertEquals("Glorious Pharaoh, your rule is unmatched! Through wisdom, balance, and strong leadership, you have ushered in a Golden Age for Egypt. The people prosper, the temples shine, and your name will be spoken with reverence for centuries to come. Your dynasty stands eternal, a beacon of prosperity and order.", goldenAgeEnding.getDescription());
+        assertEquals("endings/golden-age-ending.png", goldenAgeEnding.getImage());
+    }
+
+    @Test
+    void parseBadEnding_ValidJson_ReturnsCorrectEnding() throws Exception {
+        String json = "{ \"bad ending\": { " +
+                "\"description\": \"The people have lost faith in your leadership. Chaos reigns, and your dynasty is brought to an untimely end. The once-glorious temples now lie in ruin, and your name will be forgotten by history. You have failed Egypt.\"," +
+                "\"image\": \"endings/bad-ending.png\" } }";
+        ObjectMapper mapper = new ObjectMapper();
+
+        Ending badEnding = mapper.convertValue(
+                mapper.readTree(json).get("bad ending"),
+                Ending.class
+        );
+        assertEquals("The people have lost faith in your leadership. Chaos reigns, and your dynasty is brought to an untimely end. The once-glorious temples now lie in ruin, and your name will be forgotten by history. You have failed Egypt.", badEnding.getDescription());
+        assertEquals("endings/bad-ending.png", badEnding.getImage());
     }
 }
