85cedfbb201aad81905ef43629f5eb76a66c09c5
JasperHv <jbvanheusden@gmail.com>
2025-06-29 19:33:05 +0200

    test: HandleInfluencePillars tests

--- src/test/java/ancientegyptiansgame/game/GameplaySimulationTestSuite.java
+++ src/test/java/ancientegyptiansgame/game/GameplaySimulationTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @SelectClasses({
         HandleScoreUnitTests.class,
+        HandleInfluencePillarsUnitTests.class,
 })
 public class GameplaySimulationTestSuite {
     // no code needed, annotations do the work
